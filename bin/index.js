b3e1bf3f1b8c7c5c485dfbe6ed5cdf78060fd587
James <james.speirs@gmail.com>
2017-06-22 10:09:56 -0600

    add nav instruction

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,6 @@
 'use strict';
 const blessed       = require('blessed');
+const byuOauth      = require('byu-wabs-oauth');
 const common        = require('./common');
 const config        = require('./config');
 const fs            = require('fs');
@@ -59,7 +60,8 @@ const apps = blessed.box({
     bg: colors.blur.bg,
     fg: colors.blur.fg,
     padding: 1,
-    width: '28%'
+    width: '28%',
+    bottom: 2
 });
 
 //////////////////////////////
@@ -83,6 +85,7 @@ create.run = function() {
     };
     list.add(item);
     list.active = item;
+    views.show();
     store.views.show(actions.edit);
     edit.items.name.input.focus();
     screen.render();
@@ -91,13 +94,27 @@ create.run = function() {
 create.key(['down', 'enter'], (ch, key) => {
     switch (key.name) {
         case 'down':
-            list.focus();
-            screen.render();
+            if (list.length > 0) {
+                list.focus();
+                screen.render();
+            }
             break;
         case 'enter':
             create.run();
             break;
     }
+});
+
+create.on('focus', () => {
+    const keys = { enter: 'Create New' };
+    if (list.length > 0) {
+        keys.down = 'App List';
+    } else {
+        list.hide();
+    }
+    instruction.keys(keys);
+    actions.hide();
+    store.views.show(null);
 });
 
 create.on('click', () => {
@@ -130,11 +147,8 @@ list.save = function() {
 list.load = function() {
     const data = config.read();
     Object.keys(data).forEach(key => {
-        //list.add(Object.assign({}, data[key], { name: key }));
+        list.add(Object.assign({}, data[key], { name: key }));
     });
-    for (let i = 0; i < 10; i++) {
-        list.add({ name: 'num-' + i, consumerKey: 'key-' + i, consumerSecret: 'sec-' + i, encryptSecret: 'enc-' + i });
-    }
 };
 
 list.on('focus', active => {
@@ -142,19 +156,26 @@ list.on('focus', active => {
         actions.show();
         views.show();
         store.views.show(null);
+
+        actions.title.setContent('{bold}' + (active ? active.item.name : '') + '{/bold}');
+
+        const form = edit.items;
+        form.name.value = active ? active.item.name : '';
+        form.consumerKey.value = active ? active.item.consumerKey : '';
+        form.consumerSecret.value = active ? active.item.consumerSecret : '';
+        form.encryptSecret.value = active ? active.item.encryptSecret : '';
+
+        instruction.keys({
+            up: list.index === 0 ? 'Create Button' : 'Previous',
+            down: active.index + 1 < list.length ? 'Next' : '',
+            right: 'App Menu',
+            enter: 'App Menu'
+        });
+
         screen.render();
+    } else {
+        create.focus();
     }
-});
-
-list.on('change', active => {
-    actions.title.setContent('{bold}' + (active ? active.item.name : '') + '{/bold}');
-
-    edit.items.name.value = active ? active.item.name : '';
-    edit.items.consumerKey.value = active ? active.item.consumerKey : '';
-    edit.items.consumerSecret.value = active ? active.item.consumerSecret : '';
-    edit.items.encryptSecret.value = active ? active.item.encryptSecret : '';
-
-    screen.render();
 });
 
 list.key(['up', 'down', 'right', 'enter'], (ch, key) => {
@@ -189,6 +210,7 @@ const actions = blessed.box({
     padding: 1,
     left: '28%',
     width: '28%',
+    bottom: 2,
     scrollable: true
 });
 
@@ -261,7 +283,8 @@ const views = blessed.box({
     fg: colors.blur.fg,
     padding: 1,
     top: 0,
-    left: '56%'
+    left: '56%',
+    bottom: 2
 });
 
 //////////////////////////////
@@ -289,7 +312,6 @@ const build = common.form({
         }
     },
     cancel: function() {
-
         if (store.mode === KEY) {
             store.actions.index = 0;
         } else {
@@ -341,32 +363,58 @@ const edit = common.form({
         consumerSecret: 'Consumer Secret',
         encryptSecret: 'Encrypt Secret'
     },
-    submit: function(data) {
-        const d = data.textbox;
-        const name = d[0];
+    submit: {
+        label: 'Save',
+        handler: function(data) {
+            const d = data.textbox;
+            const name = d[0];
+            const nameChange = list.active.name !== name;
 
-        // verify that name is valid
-        if (name.length === 0) return screen.message('App name required');
-        if (name.length > 30) return screen.name('App name must be 30 characters or less');
-        if (!/^[A-Z0-9-]+$/.test(name)) return screen.message('App name must be alpha-numeric and can have dashes');
-        if (list.active.name !== name && !list.nameAvailable(name)) return screen.message('App name already in use');
+            // verify that name is valid
+            if (name.length === 0) return screen.message('App name required');
+            if (name.length > 30) return screen.name('App name must be 30 characters or less');
+            if (!/^[a-zA-Z0-9-]+$/.test(name)) return screen.message('App name must be alpha-numeric and can have dashes');
+            if (nameChange && !list.nameAvailable(name)) return screen.message('App name already in use');
 
-        // update in memory store
-        if (list.active.$new) delete list.active.$new;
-        list.update(list.active, {
-            name: name,
-            consumerKey: d[1],
-            consumerSecret: d[2],
-            encryptSecret: d[3]
-        });
+            // update in memory store
+            if (list.active.$new) delete list.active.$new;
+            list.update(list.active, {
+                name: name,
+                consumerKey: d[1],
+                consumerSecret: d[2],
+                encryptSecret: d[3]
+            });
 
-        list.save();
+            if (nameChange) actions.title.setContent('{bold}' + name + '{/bold}');
 
-        store.actions.index = 1;
+            list.save();
+            screen.message('Edits saved.');
+
+            const active = list.active;
+            if (active && active.consumerKey && active.consumerSecret) {
+                test.wso2(active.consumerKey, active.consumerSecret)
+                    .then(success => {
+                        screen.message('Edits saved. WSO2 consumer key and secret are ' + (success ? '' : 'not ') + 'valid.');
+                    });
+            }
+
+            list.show();
+            if (store.mode === KEY) {
+                store.actions.index = 1;
+            } else {
+                list.focus();
+            }
+        }
     },
     cancel: function() {
         if (list.active.$new) list.remove(list.active);
-        list.focus();
+        if (list.length === 0) {
+            create.focus();
+        } else if (store.mode === KEY) {
+            store.actions.index = 1;
+        } else {
+            list.focus();
+        }
     }
 });
 store.views.set(actions.edit, edit);
@@ -382,14 +430,45 @@ const test = common.form({
     title: 'Test WSO2',
     submit: {
         label: 'Test',
-        handler: function(data) {
+        handler: function() {
+            const active = list.active;
+            if (!active) return screen.message('App to test WSO2 key and secret not specified.');
+            if (!active.consumerKey) return screen.message('App missing consumer key.');
+            if (!active.consumerSecret) return screen.message('App missing consumer secret.');
 
+            test.wso2(active.consumerKey, active.consumerSecret)
+                .then(success => {
+                    screen.message('WSO2 consumer key and secret are ' + (success ? '' : 'not ') + 'valid.');
+                });
+
+            if (store.mode === KEY) {
+                store.actions.index = 2;
+            } else {
+                list.focus();
+            }
         }
     },
     cancel: function() {
-        list.focus();
+        if (store.mode === KEY) {
+            store.actions.index = 2;
+        } else {
+            list.focus();
+        }
     }
 });
+
+test.wso2 = function(consumerKey, consumerSecret) {
+    const oauth = byuOauth(consumerKey, consumerSecret, 'https://api.byu.edu/.well-known/openid-configuration');
+    screen.message('Testing WSO@ consumer key and secret now. Please wait...');
+    return oauth.getClientGrantAccessToken()
+        .then(function(token) {
+            return token && token.accessToken && true;
+        })
+        .catch(() => {
+            return false;
+        });
+};
+
 test.append(blessed.box({
     bg: colors.blur.bg,
     fg: colors.blur.fg,
@@ -426,13 +505,18 @@ const duplicate = common.form({
             list.add(item);
             list.sort();
             list.save();
+            screen.message('Duplicate saved.');
 
             list.active = item;
             list.focus();
         }
     },
     cancel: function() {
-        list.focus();
+        if (store.mode === KEY) {
+            store.actions.index = 3;
+        } else {
+            list.focus();
+        }
     }
 });
 const duplicateError = blessed.box({
@@ -455,59 +539,40 @@ const deleteApp = common.form({
     title: 'Delete App',
     submit: {
         label: 'Confirm',
-        handler: function(data) {
-
+        handler: function() {
+            list.remove(list.active);
+            list.focus();
+            list.save();
+            screen.message('Delete saved.');
         }
     },
     cancel: function() {
-        list.focus();
+        if (list.length === 0) {
+            list.hide();
+            create.focus();
+        } else {
+            if (store.mode === KEY) {
+                store.actions.index = 4;
+            } else {
+                list.focus();
+            }
+        }
     }
 });
 store.views.set(actions.delete, deleteApp);
 
-
-
-
-
-
-
 //////////////////////////////
-//                          //
-//      MESSAGE BOX         //
-//                          //
+//      INSTRUCTION         //
 //////////////////////////////
 
-// message box
-const message = blessed.box({
+const instruction = common.instruction({
     parent: screen,
-    left: 2,
-    right: 2,
-    bottom: 2,
-    height: 3,
-    padding: 1,
-    bg: 230,
-    fg: 232,
-    align: 'center'
+    bottom: 0,
+    height: 3
 });
-message.timeoutId = null;
 
-message.hide();
-
-screen.message = function(data, time) {
-    if (data && typeof data === 'object') {
-        data = JSON.stringify(data);
-    } else if (typeof data !== 'string') {
-        data = String(data);
-    }
-    message.setContent(data);
-    message.show();
-    clearTimeout(message.timeoutId);
-    message.timeoutId = setTimeout(() => {
-        message.hide();
-        screen.render();
-    }, time || 3000);
-    screen.render();
-};
+screen.instruction = instruction;
+screen.message = instruction.message;
 
 
 
@@ -518,10 +583,26 @@ screen.message = function(data, time) {
 //////////////////////////////
 
 store.actions.items.forEach((btn, index) => {
+    const length = store.actions.items.length;
+    const label = btn.getContent().split(' ')[0];
+    const keys = {
+        up: index > 0 ? 'Previous' : '',
+        down: index + 1 < length ? 'Next' : '',
+        left: 'App List',
+        right: label,
+        enter: label,
+        esc: 'App List'
+    };
+
     btn.on('press', () => {
         store.actions.index = index;
         store.views.active.view.focus();
         screen.render();
+    });
+
+    btn.on('focus', () => {
+        actions.show();
+        instruction.keys(keys);
     });
 
     btn.key(['up', 'down', 'left', 'right', 'escape', 'enter'], (ch, key) => {
@@ -546,9 +627,7 @@ store.actions.items.forEach((btn, index) => {
 });
 
 // Quit on Escape, q, or Control-C.
-screen.key(['q', 'C-c'], function(ch, key) {
-    return process.exit(0);
-});
+screen.key(['q', 'S-q', 'C-c'], () => process.exit(0));
 
 
 
@@ -566,6 +645,7 @@ if (list.length > 0) {
     actions.title.setContent('{bold}' + list.active.name + '{/bold}');
     list.focus();
 } else {
+    list.hide();
     create.focus();
 }
 
