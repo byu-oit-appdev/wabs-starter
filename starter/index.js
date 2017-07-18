fabb590a04cc901b651113aee3b75f1c1717db7f
James <james.speirs@gmail.com>
2017-07-18 13:17:00 -0600

    enable server hot reload w/o restart

--- starter/index.js
+++ starter/index.js
@@ -15,8 +15,9 @@
  *    limitations under the License.
  **/
 'use strict';
+const http              = require('http');
+const path              = require('path');
 const pkg               = require('./package.json');
-const server            = require('./server/index');
 const wabs              = require('byu-wabs');
 
 process.on('unhandledRejection', e => {
@@ -26,4 +27,86 @@ process.on('unhandledRejection', e => {
 
 console.log('ENV: ' + process.env.NODE_ENV);
 
-wabs.getOptions(pkg.name).then(server);
+wabs.getOptions(pkg.name)
+    .then(options => {
+        console.log(' ');
+        const devMode = process.env.NODE_ENV === 'development';
+        let app;
+        let dirty;
+
+        const server = http.createServer(function(req, res, next) {
+            app(req, res, next);
+        });
+
+        const listener = server.listen(process.env.WABS_PORT, function(err) {
+            if (err) {
+                console.error(err.stack);
+                process.exit(1);
+            } else {
+                console.log('Server listening on port ' + listener.address().port + '\n');
+                load();
+            }
+        });
+
+        if (devMode) {
+            console.log('Starting server app hot reload\n');
+
+            const chokidar = require('chokidar');
+            let ready = false;
+            let timeoutId;
+            dirty = {};
+
+            chokidar.watch(path.resolve(__dirname, 'server'))
+                .on('all', (event, partialPath) => {
+                    if (!ready) return;
+                    switch (event) {
+                        case 'add':
+                        case 'change':
+                        case 'unlink':
+                            if (/.+\.js$/.test(partialPath)) {
+                                const fullPath = path.resolve(__dirname, partialPath);
+                                clearTimeout(timeoutId);
+                                timeoutId = setTimeout(load, 300);
+                                if (!dirty[fullPath]) dirty[fullPath] = null;
+                            }
+                            break;
+                    }
+                })
+                .on('ready', () => {
+                    ready = true;
+                    console.log('Server app hot reload ready\n');
+                });
+        }
+
+        function error(req, res, next) {
+            res.write('Internal Server Error');
+            res.end();
+        }
+
+        function load() {
+
+            // clear require cache for changed files
+            const cache = require.cache;
+            Object.keys(dirty).forEach(path => {
+                dirty[path] = cache[path];
+                delete cache[path]
+            });
+
+            // attempt to load the app
+            console.log('Server app ' + (app ? 're' : '') + 'loading...\n');
+            try {
+                app = require('./server/index')(options);
+                console.log('========================================\nServer app loaded\n');
+                return true;
+            } catch (err) {
+                app = error;
+                console.error('Could not load server app:');
+                console.error(err.stack + '\n');
+
+                if (devMode) {
+                    console.error('The server is still running, you do not need to restart it. ' +
+                        'Fix the error and save for automatic reload.\n');
+                }
+            }
+        }
+    });
