5a7c466cc796278db5ad0bbb962f0d113d41f0ab
James Speirs <james.speirs@gmail.com>
2018-02-09 12:33:05 -0700

    produce a simple home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template>
-    <h2>Home Page</h2>
-    <p>This is the home page</p>
+    <div>
+        <h2>Home Page</h2>
+        <p>This is the home page</p>
+    </div>
 </template>
 
 <script>
@@ -12,4 +14,5 @@
         font-weight: bold;
         color: orange;
     }
+
 </style>
