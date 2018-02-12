ae2fe0bbda8cb1993019b1c24a4ba7648301e014
James Speirs <james.speirs@gmail.com>
2018-02-12 15:24:45 -0700

    define page navigation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,7 +6,20 @@
 </template>
 
 <script>
+    export default {
 
+        // define a new navigation when this page is loaded
+        byuNavigation(route, links) {
+            return [
+                { href: '/', title: 'Home' },
+                { href: '/link-2', title: 'Link 2' },
+                { href: '/link-3', title: 'Link 3' },
+                { href: '/link-4', title: 'Link 4' },
+                { href: '/link-5', title: 'Link 5' },
+                { href: '/link-6', title: 'Link 6' }
+            ]
+        }
+    }
 </script>
 
 <style scoped>
